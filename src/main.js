109d82352bc7639762f90a5d5b174aca3b046785
kayne@unnotech.com <kayne@unnotech.com>
2018-02-06 15:39:17 +0800

    minor

--- src/main.js
+++ src/main.js
@@ -27,7 +27,9 @@ if (params.r) {
   let expires = new Date()
   expires.setMonth(expires.getMonth() + 1)
   VueCookie.set('r', params.r, {expires: expires})
-} else if (params.desktop === '0' && Vue.cookie.get('desktop') !== '0') {
+}
+
+if (params.desktop === '0' && Vue.cookie.get('desktop') !== '0') {
   VueCookie.set('desktop', params.desktop)
   window.location.reload()
 }
