d503086b3f07880e1ac6590107e656118416dda5
kayne@unnotech.com <kayne@unnotech.com>
2018-03-09 17:13:08 +0800

    fix conflict

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,8 +4,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  HOST: '"/api"',
-  // HOST: '"http://2e2d1a08-4238-4165-9703-5f6a955fb6df.node.dockerapp.io:8003"',
+  HOST: '"http://2e2d1a08-4238-4165-9703-5f6a955fb6df.node.dockerapp.io:8003"',
   chatHost: '"ws://2e2d1a08-4238-4165-9703-5f6a955fb6df.node.dockerapp.io:8003"',
   ghost: '"http://a546542.eastasia.cloudapp.azure.com:8000"',
   decode_key: '"61Q3hC6jEvfQrwQvMd80fPm2XEqDPJhB"'
